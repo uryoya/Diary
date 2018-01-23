b07e6e3cccb2796e1e59c70d5ee9c4d4f6ef1969
URANO Ryoya <urano.works.mail@gmail.com>
2018-01-23 16:58:00 +0900

    Diaryエンティティを定義

---
+++ src/main/scala/com/uryoya/diary/entity/mysql/Diary.scala
@@ -0,0 +1,13 @@
+package com.uryoya.diary.entity.mysql
+
+import java.sql.Timestamp
+import com.uryoya.diary.entity.{DiaryId, UserId}
+
+case class Diary (
+  id: DiaryId,
+  authorId: UserId,
+  title: String,
+  body: String,
+  createAt: Timestamp,
+  updateAt: Timestamp,
+)
