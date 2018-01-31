f88e72c04cea5c2eea61a693d2078a8c8d845329
URANO Ryoya <urano.works.mail@gmail.com>
2018-01-31 16:02:11 +0900

    サインインのAPI(仮)

--- src/main/scala/com/uryoya/diary/Api.scala
+++ src/main/scala/com/uryoya/diary/Api.scala
@@ -1,8 +1,11 @@
 package com.uryoya.diary
 
 import com.twitter.finagle.Service
-import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.http.{Cookie, Request, Response}
+import com.uryoya.diary.controller.AuthenticationController
 import com.uryoya.diary.entity.{HelloWorldRequest, HelloWorldResponse}
+import com.uryoya.diary.request.SigninRequest
+import com.uryoya.diary.response.MessageResponse
 import io.circe.generic.auto._
 import io.finch.Endpoint
 import io.finch._
@@ -10,6 +13,20 @@ import io.finch.circe._
 
 class Api {
   val service: Service[Request, Response] = {
+    val session: Endpoint[String] = root.map { req =>
+      req.cookies
+        .get("SESSION")
+        .headOption.map(_.value.toString)
+        .getOrElse("")
+    }
+
+    val signin: Endpoint[MessageResponse] =
+      post("api" :: "signin" :: jsonBody[SigninRequest]) { req: SigninRequest =>
+        AuthenticationController.Signin(req) match {
+          case Right(res) => Ok(res).withCookie(new Cookie("SESSION", "SESSIONID"))
+          case Left(e) => BadRequest(new IllegalArgumentException(e.message))
+        }
+      }
     // $ curl localhost:8080/helloworld
     // {"hello":"urano"}
     val helloWorld1: Endpoint[HelloWorldResponse] =
@@ -20,10 +37,12 @@ class Api {
     // $ curl -d '{"hello":"urano"}' localhost:8080/hello/world
     // {"hello":"urano"}
     val helloWorld2: Endpoint[HelloWorldResponse] =
-      post("hello" :: "world" :: jsonBody[HelloWorldRequest]) { req: HelloWorldRequest =>
-        Ok(HelloWorldResponse(hello = req.hello))
+      post("hello" :: "world" :: jsonBody[HelloWorldRequest] :: session) { (req: HelloWorldRequest, session: String) =>
+        val cookie = new Cookie("kafu", "chino")
+        println(session)
+        Ok(HelloWorldResponse(hello = req.hello)).withCookie(cookie)
       }
 
-    (helloWorld1 :+: helloWorld2).toServiceAs[Application.Json]
+    (signin :+: helloWorld1 :+: helloWorld2).toServiceAs[Application.Json]
   }
 }
