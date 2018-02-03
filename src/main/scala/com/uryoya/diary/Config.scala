e060de41dd2b2576f6aa2ef3972f40fc38878a6f
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-03 23:54:53 +0900

    redisの設定を追加

--- src/main/scala/com/uryoya/diary/Config.scala
+++ src/main/scala/com/uryoya/diary/Config.scala
@@ -7,11 +7,13 @@ final case class Config(
   mysql: MysqlConfig,
   redis: RedisConfig,
   github: GitHubAppKeys,
+  session: SessionConfig,
 )
 
 object Config {
   def load: Config = loadConfigOrThrow[Config]
 
+  final case class SessionConfig(cookieName: String = "SID", maxAge: Option[Int])
   final case class MysqlConfig(host: String, port: Int, user: String, password: String, db: String)
   final case class RedisConfig(host: String, port: Int)
   final case class GitHubAppKeys(clientId: String, clientSecret: String)
