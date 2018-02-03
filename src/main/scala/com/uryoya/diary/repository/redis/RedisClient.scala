c3ef7ca7e12c18cf1dfb3f45b07a966f0e63e65d
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-03 23:50:51 +0900

    finalge.redis.Clientの薄いラッパー(仮)

---
+++ src/main/scala/com/uryoya/diary/repository/redis/RedisClient.scala
@@ -0,0 +1,32 @@
+package com.uryoya.diary.repository.redis
+
+import com.twitter.finagle.redis
+import com.twitter.finagle.redis.util.{BufToString, StringToBuf}
+import com.twitter.io.Buf
+import com.twitter.util.Future
+
+class RedisClient(underlying: redis.Client) {
+  private implicit def anyToBuf: Any => Buf = a => StringToBuf(a.toString)
+
+  def get(key: String): Future[Option[String]] = {
+    underlying.get(StringToBuf(key)).map{ r =>
+      r.map(buf => BufToString(buf))
+    }
+  }
+
+  def hGetAll(key: String): Future[Map[String, String]] = {
+    underlying.hGetAll(StringToBuf(key)).map { r =>
+      r.map{
+        case(k, v) => (BufToString(k), BufToString(v))
+      }.toMap
+    }
+  }
+
+  def hSet(key: String, field: String, value: String): Future[java.lang.Long] = {
+    underlying.hSet(StringToBuf(key), StringToBuf(field), StringToBuf(value))
+  }
+
+  def del(key: String): Future[java.lang.Long] = {
+    underlying.dels(Seq(StringToBuf(key)))
+  }
+}
