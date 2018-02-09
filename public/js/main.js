43d34ca42ec7a68540034020c1d474953d5dab65
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-10 01:47:05 +0900

    PUT/api/users/:login_id APIのテスト

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 function signin() {
+    const form = new FormData(document.querySelector('#signin-form'));
     const data = {
-        "login": "uryoya",
-        "password": "pass"
+        "login": form.get('login'),
+        "password": form.get('password'),
     };
     fetch('/api/signin', {
         method: 'post',
@@ -12,7 +13,6 @@ function signin() {
             if (resp.ok) {
                 console.log("OK");
             } else {
-                console.log("BUUUUUU");
                 console.log(resp);
             }
         })
@@ -64,10 +64,7 @@ function getUser() {
         console.log(user)
     });
 }
-
-function getAllUser() {
-    fetch('/api/users', {
-        credentials: 'include'
+function getAllUser() { fetch('/api/users', { credentials: 'include'
     })
     .then(function(resp) {
         return resp.json()
@@ -77,8 +74,30 @@ function getAllUser() {
     });
 }
 
+function updateUser() {
+    const login_id = document.getElementById('form-login-id').value;
+    const form = new FormData(document.getElementById('update-user-form'));
+    const data = {};
+    if (form.get('name') !== '') data['name'] = form.get('name');
+    if (form.get('access-token') !== '') data['access-token'] = form.get('access-token');
+    if (form.get('password') !== '') data['password'] = form.get('password');
+    console.log(data);
+    fetch(`/api/users/${login_id}`, {
+        credentials: 'include',
+        method: 'put',
+        body: JSON.stringify(data)
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function (user) {
+       console.log(user);
+    })
+}
+
 document.querySelector('#signin').addEventListener("click", signin);
 document.querySelector('#signout').addEventListener('click', signout);
 document.querySelector('#create-user').addEventListener('click', createUser);
 document.querySelector("#get-user").addEventListener("click", getUser);
 document.querySelector('#get-users').addEventListener('click', getAllUser);
+document.querySelector('#update-user-button').addEventListener('click', updateUser);
