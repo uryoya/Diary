a9c8dfcf77cb2c74d2471e7176dcf4af26284f56
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-09 14:41:31 +0900

    AUTO_INCREMENT/UNIQUE制約を追加

--- src/main/resources/diary.sql
+++ src/main/resources/diary.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT NOT NULL,
-  `login` VARCHAR(64) NOT NULL,         # GitHub login name
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(64) NOT NULL UNIQUE,  # GitHub login name
   `password_hash` VARCHAR(64) NOT NULL,
   `name` VARCHAR(64) NOT NULL,          # Account name
   `avatar_uri` VARCHAR(128) NOT NULL,   # Avatar icon's uri (directory path or link)
