139d068895762fd757e6657fc4d7af42ed73d6e3
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-12 03:33:36 +0900

    リサイズ後のサイズを指定

--- src/main/scala/com/uryoya/diary/Config.scala
+++ src/main/scala/com/uryoya/diary/Config.scala
@@ -14,8 +14,11 @@ final case class Config(
 object Config {
   def load: Config = loadConfigOrThrow[Config]
 
-  final case class UserAvatarConfig(localRootPath: String,  // 画像を保存するディレクトリ
-                                    serverRootPath: String) // サーバから取得するときの基底パス
+  final case class UserAvatarConfig(
+                                     localRootPath: String,  // 画像を保存するディレクトリ
+                                     serverRootPath: String, // サーバから取得するときの基底パス
+                                     edgeSize: Int,          // 圧縮後の1辺の長さ
+                                   )
   final case class SessionConfig(cookieName: String, maxAge: Long)
   final case class MysqlConfig(host: String, port: Int, user: String, password: String, db: String)
   final case class RedisConfig(host: String, port: Int)
