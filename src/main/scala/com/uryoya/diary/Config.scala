7f9033c66e4aa78fc1316280788439bcc1dc1577
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-11 18:27:53 +0900

    アバターアイコンを保存する場所の設定

--- src/main/scala/com/uryoya/diary/Config.scala
+++ src/main/scala/com/uryoya/diary/Config.scala
@@ -4,6 +4,7 @@ import com.uryoya.diary.Config._
 import pureconfig._
 
 final case class Config(
+  userAvatar: UserAvatarConfig,
   mysql: MysqlConfig,
   redis: RedisConfig,
   github: GitHubAppKeys,
@@ -13,6 +14,8 @@ final case class Config(
 object Config {
   def load: Config = loadConfigOrThrow[Config]
 
+  final case class UserAvatarConfig(localRootPath: String,  // 画像を保存するディレクトリ
+                                    serverRootPath: String) // サーバから取得するときの基底パス
   final case class SessionConfig(cookieName: String, maxAge: Long)
   final case class MysqlConfig(host: String, port: Int, user: String, password: String, db: String)
   final case class RedisConfig(host: String, port: Int)
