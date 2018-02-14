fcd2e85df3faf81b970c62cf3c4a3d2d32fde1da
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-14 16:45:22 +0900

    Diaryを更新するAPIのために変更

--- src/main/scala/com/uryoya/diary/request/DiaryRequest.scala
+++ src/main/scala/com/uryoya/diary/request/DiaryRequest.scala
@@ -1,6 +1,6 @@
 package com.uryoya.diary.request
 
 final case class DiaryRequest (
-  title: String,
-  body: String,
+  title: Option[String],
+  body: Option[String],
 )
---
+++ src/main/scala/com/uryoya/diary/request/PostDiaryRequest.scala
@@ -0,0 +1,6 @@
+package com.uryoya.diary.request
+
+final case class PostDiaryRequest (
+  title: String,
+  body: String,
+)
