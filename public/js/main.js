d57e028152aa69ed07ebfd32dea131486be9d82a
URANO Ryoya <urano.works.mail@gmail.com>
2018-02-22 19:50:51 +0900

    日誌一覧をとりあえず取得するだけ

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+fetch('/api/myself', {
+    method: "get",
+    credentials: 'include'
+}).then(function(resp) {
+    if (resp.ok) {
+        return resp.json();
+    } else {
+        throw new Error('user not signined.')
+    }
+}).then(function (myself) {
+    new Vue({
+        el: '#account-info',
+        data: {
+            name: myself.name,
+        },
+    });
+
+    let diaries = new Vue({
+        el: '#diary-list',
+        data: {
+            diaries: [],
+        },
+        created: function () {
+            this.getDiaries();
+        },
+        methods: {
+            getDiaries: async function () {
+                let diaries = await fetch('/api/diaries', {
+                    credentials: 'include'
+                }).then(function(resp) {
+                    return resp.json()
+                });
+                this.diaries = diaries;
+            }
+        }
+    });
+
+    window.diaries = diaries;
+}).catch(function (err) {
+    console.log(err);
+    window.location.href = '/Diary/public/test.html';
+});
